d8f6782b9508a9ad8884f72bc2cd69d747777637
chris <chris@foo.foo>
2025-11-01 09:18:42 +0200

    Created for and cloned locally. Custom Config: QWERTY, NAV=VI

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+
+// custom_config.h
+#define MIRYOKU_ALPHAS=QWERTY
+#define MIRYOKU_TAP=QWERTY
+#define MIRYOKU_NAV=VI
+
+//#define MIRYOKU_EXTRA=COLEMAKDH
+//#define MIRYOKU_NAV=INVERTEDT
+//#define MIRYOKU_CLIPBOARD=WIN
+//#define MIRYOKU_LAYERS=FLIP
+//#define MIRYOKU_MAPPING=EXTENDED_THUMBS
